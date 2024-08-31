e466e9475f265a7f6b13163266fc4988d36c1624
Hostyn <ruben@martinezhostyn.com>
2024-08-31 11:17:16 +0200

    Update ips

--- modules/services/k3s.nix
+++ modules/services/k3s.nix
@@ -12,18 +12,19 @@ in
     networking.firewall.allowedTCPPorts = [ 6443 2379 2380 ];
     networking.firewall.allowedUDPPorts = [ 8472 ];
 
+    # You will need to add kube-vip manually https://kube-vip.io/docs/usage/k3s/
     services.k3s = {
       enable = true;
       role = "server";
       token = "awQ4VyY6bdJUGj1l";
       clusterInit = true;
-      serverAddr = "https://192.168.1.60:6443";
+      serverAddr = "https://192.168.0.200:6443";
       extraFlags = toString [
         "--write-kubeconfig-mode \"0644\""
         "--disable servicelb"
         "--disable traefik"
         "--disable local-storage"
-        "--tls-san 192.168.1.60"
+        "--tls-san 192.168.0.200"
       ];
     };
   };
