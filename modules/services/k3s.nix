a5a4a8ca296686bf793131edfe34d4322652cab6
Hostyn <ruben@martinezhostyn.com>
2024-08-31 10:19:22 +0200

    Open k3s ports

--- modules/services/k3s.nix
+++ modules/services/k3s.nix
@@ -9,6 +9,9 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    networking.firewall.allowedTCPPorts = [ 6443 2379 2380 ];
+    networking.firewall.allowedUDPPorts = [ 8472 ];
+
     services.k3s = {
       enable = true;
       role = "server";
